b686ed237d691c270a10406778d90c3a50e6dbfd
Bojan Panic <bojan@Bojans-MacBook-Air.local>
2021-10-27 12:19:07 +0200

    Add message component, add sample message to chatBox component

--- src/Components/ChatBox.tsx
+++ src/Components/ChatBox.tsx
@@ -1,3 +1,14 @@
+import { Message } from "./Message";
+
 export const ChatBox = () => {
-  return <div>Chat box</div>;
+  return (
+    <div>
+      <Message
+        text="This is a message text, lorem ispum text"
+        color="#fff"
+        name="Author"
+        date="10.10.2010"
+      />
+    </div>
+  );
 };
--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -1,3 +1,16 @@
-export const Message = () => {
-  return <div>Message</div>;
+interface IProps {
+  text: string;
+  name?: string;
+  date: string;
+  color: string;
+}
+
+export const Message = (props: IProps) => {
+  return (
+    <div style={{ background: props.color }}>
+      {props.name ? <div>{props.name}</div> : null}
+      <div>{props.text}</div>
+      <div>{props.date}</div>
+    </div>
+  );
 };
