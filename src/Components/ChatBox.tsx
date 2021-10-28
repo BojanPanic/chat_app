8756bc9ea6d1d6f4185e0fbdb3b2b64317e29e15
Bojan Panic <bojan@Bojans-MacBook-Air.local>
2021-10-28 11:15:55 +0200

    Add styles for chatbox component, add logic to check if user sent a message

--- src/Components/ChatBox.tsx
+++ src/Components/ChatBox.tsx
@@ -3,16 +3,23 @@ import { formatDate } from "helpers/helper";
 import { useEffect, useState } from "react";
 import { IMessage } from "types/types";
 import { Message } from "./Message";
+import "styles/chatbox.scss";
 
-export const ChatBox = () => {
+interface IProps {
+  username: string;
+}
+
+export const ChatBox = (props: IProps) => {
   const [messages, setMessages] = useState<IMessage[]>([]);
+
+  const isAuthorsMessage = (messageAuthor: string, username: string): boolean =>
+    messageAuthor.toLowerCase() === username.toLowerCase();
 
   const fetchMessages = async () => {
     const data: any = await axios({
       method: "GET",
       url: `https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/?&limit=10&token=${process.env.REACT_APP_API_KEY}`,
     });
-    console.log(data);
     setMessages(data.data);
   };
 
@@ -36,20 +43,34 @@ export const ChatBox = () => {
   }, []);
 
   return (
-    <div>
-      {messages.length &&
-        messages.map((item: IMessage) => {
-          return (
-            <div>
-              <Message
-                text={item.message}
-                color="#fff"
-                name={item.author}
-                date={formatDate(item.timestamp)}
-              />
-            </div>
-          );
-        })}
+    <div className="chatbox-wrap">
+      <div className="chatbox">
+        {messages.length &&
+          messages.map((item: IMessage) => {
+            return (
+              <div
+                key={item._id}
+                className={
+                  isAuthorsMessage(item.author, props.username)
+                    ? "author-message"
+                    : ""
+                }
+              >
+                <Message
+                  text={item.message}
+                  /* *TODO change to css class toggle  */
+                  color={
+                    item.author.toLowerCase() === props.username.toLowerCase()
+                      ? "#fcf6c3"
+                      : "#fff"
+                  }
+                  name={item.author}
+                  date={formatDate(item.timestamp)}
+                />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
